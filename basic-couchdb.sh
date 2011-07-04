3cdb415d29891d78d44998a53bdc66ae37b6b945
Philipe Farias <philipefarias@gmail.com>
2011-07-03 22:32:10 -0300

    setting ssh conf with the of the StackScript 123, lib-system-ubuntu

--- basic-couchdb.sh
+++ basic-couchdb.sh
@@ -8,26 +8,41 @@
 # <udf name="user_password" label="Unprivileged User Password" />
 # <udf name="user_sshkey" label="Public Key for User" default="" />
 #
-# <udf name="sshd_port" label="SSH Port" default="22" />
-# <udf name="sshd_protocol" label="SSH Protocol" oneOf="1,2,1 and 2" default="2" />
-# <udf name="sshd_permitroot" label="SSH Permit Root Login" oneof="No,Yes" default="No" />
-# <udf name="sshd_passwordauth" label="SSH Password Authentication" oneOf="No,Yes" default="No" />
-# <udf name="sshd_group" label="SSH Allowed Groups" default="sshusers" example="List of groups seperated by spaces" />
-#
-# <udf name="sudo_usergroup" label="Usergroup to use for Admin Accounts" default="wheel" />
-# <udf name="sudo_passwordless" label="Passwordless Sudo" oneof="Require Password,Do Not Require Password", default="Require Password" />
+# <UDF name="sshd_permitrootlogin" label="Permit SSH root login" oneof="No,Yes" default="No" />
+# <UDF name="sshd_passwordauth" label="Use SSH password authentication" oneOf="Yes,No" default="Yes" example="Turn off password authentication if you have added a Public Key" />
+# <UDF name="sys_hostname" Label="System hostname" default="myvps" example="Name of your server, i.e. linode1" />
 #
 # <udf name="couch_version" label="Apache CouchDB Version" default="1.1.0" />
 # <udf name="couch_mirror" label="Apache CouchDB Package Mirror" default="mirror.atlanticmetro.net/apache" example="Paste the url till the couchdb folder." />
 
 source <ssinclude StackScriptID="1">
-source <ssinclude StackScriptID="165">
+source <ssinclude StackScriptID="123"> # lib-system-ubuntu
 
-apt-get -y install ufw
+system_update
+
+system_update_locale_en_US_UTF_8
+system_update_hostname "$SYS_HOSTNAME"
+
+# Create user account
+system_add_user "$USER_NAME" "$USER_PASSWORD" "$USER_GROUPS"
+if [ "$USER_SSHKEY" ]; then
+    system_user_add_ssh_key "$USER_NAME" "$USER_SSHKEY"
+fi
+
+# Configure sshd
+system_sshd_permitrootlogin "$SSHD_PERMITROOTLOGIN"
+system_sshd_passwordauthentication "$SSHD_PASSWORDAUTH"
+system_sshd_pubkeyauthentication "yes"
+service ssh restart
+
+# Install firewall and some security utilities
+apt-get -y install fail2ban logcheck logcheck-database ufw
 
 ufw default deny
 ufw allow ${SSHD_PORT}
 ufw enable
+
+postfix_install_loopback_only # SS1
 
 # Install CouchDB
 source <ssinclude StackScriptID="2847">
@@ -42,3 +57,5 @@ ufw allow 5984 # CouchDB port
 
 # Install some good stuff
 apt-get -y install bash-completion less vim wget
+
+restartServices
