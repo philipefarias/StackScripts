c3acc5fb8cdf46970f3d792ec640968cdcf6532c
Philipe Farias <philipefarias@lpj.com.br>
2011-07-04 18:41:31 -0300

    using my own lib to install and configure some system things

--- basic-couchdb.sh
+++ basic-couchdb.sh
@@ -4,61 +4,86 @@
 #
 # Author: Philipe Farias <philipefarias@gmail.com>
 #
-# <udf name="user_name" label="Unprivileged User Account" />
-# <udf name="user_password" label="Unprivileged User Password" />
+# <udf name="user_name" label="User Account" />
+# <udf name="user_password" label="User Password" />
 # <udf name="user_sshkey" label="Public Key for User" default="" />
-# <udf name="user_groups" label="User Groups" default="admin" />
 #
-# <UDF name="sshd_permitrootlogin" label="Permit SSH root login" oneof="No,Yes" default="No" />
-# <UDF name="sshd_passwordauth" label="Use SSH password authentication" oneOf="Yes,No" default="Yes" example="Turn off password authentication if you have added a Public Key" />
-# <UDF name="sys_hostname" Label="System hostname" default="myvps" example="Name of your server, i.e. linode1" />
+# <udf name="sshd_port" label="SSH Port" default="22" />
+# <udf name="hostname" label="System Hostname" default="myvps" example="Name of your server, i.e. linode1" />
+# <udf name="root_email" label="Root Email" />
 #
-# <udf name="couch_version" label="Apache CouchDB Version" default="1.1.0" />
+# <udf name="munin_server_ip" label="Munin Server IP" default="127.0.0.1" />
+#
 # <udf name="couch_mirror" label="Apache CouchDB Package Mirror" default="mirror.atlanticmetro.net/apache" example="Paste the url till the couchdb folder." />
-
-exec &> /root/stackscript.log
 
 source <ssinclude StackScriptID="1">
 source <ssinclude StackScriptID="123"> # lib-system-ubuntu
+source <ssinclude StackScriptID="2865"> # lib-system
 
 system_update
+update_locale_en_US_UTF_8
 
-system_update_locale_en_US_UTF_8
-system_update_hostname "$SYS_HOSTNAME"
-
-# Create user account
-system_add_user "$USER_NAME" "$USER_PASSWORD" "$USER_GROUPS"
-if [ "$USER_SSHKEY" ]; then
-    system_user_add_ssh_key "$USER_NAME" "$USER_SSHKEY"
-fi
-
-# Configure sshd
-system_sshd_permitrootlogin "$SSHD_PERMITROOTLOGIN"
-system_sshd_passwordauthentication "$SSHD_PASSWORDAUTH"
-system_sshd_pubkeyauthentication "yes"
-service ssh restart
+setup_hostname "$HOSTNAME"
 
 # Install firewall and some security utilities
 apt-get -y install fail2ban logcheck logcheck-database ufw
 
-ufw default deny
-ufw allow ${SSHD_PORT}
-ufw enable
 
-postfix_install_loopback_only # SS1
+# Create user account
+USER_GROUPS="admin"
+system_add_user "$USER_NAME" "$USER_PASSWORD" "$USER_GROUPS"
+if [ "$USER_SSHKEY" ]; then
+    add_user_ssh_key "$USER_NAME" "$USER_SSHKEY"
+fi
+
+# Configure sshd
+SSHD_PERMITROOTLOGIN="no"
+SSHD_PASSWORDAUTH="no"
+SSHD_PUBKEYAUTH="yes"
+configure_sshd
+
+install_postfix "$ROOT_EMAIL" "$USER_NAME"
+
+install_monit "$ROOT_EMAIL"
+install_munin_node "$HOSTNAME" "$MUNIN_SERVER_IP"
 
 # Install CouchDB
 source <ssinclude StackScriptID="2847">
-
-apt-get -y install curl build-essential
-build_spidermonkey
-
+COUCH_VERSION="1.1.0"
 curl http://${COUCH_MIRROR}/couchdb/${COUCH_VERSION}/apache-couchdb-${COUCH_VERSION}.tar.gz | tar zxv
+apt-get -y install curl build-essential
+
+build_spidermonkey
 build_couchdb "apache-couchdb-${COUCH_VERSION}"
 
+# Configure and enable firewall
+ufw logging on
+ufw default deny
+
+ufw allow ${SSHD_PORT}
 ufw allow 5984 # CouchDB port
+
+ufw enable
 
 # Install some good stuff
 apt-get -y install bash-completion less vim wget
 
-restartServices
+# Send info message
+if [ -n "$NOTIFY_EMAIL" ]; then
+  vps_hostname="`cat /etc/hostname`"
+  mail -s "Your Linode VPS "$vps_hostname" is configured" "$NOTIFY_EMAIL" <<EOD
+Hi,
+
+Your Linode VPS configuration is completed.
+
+You can now login with ssh to port "$SSHD_PORT" with user "$USER_NAME".
+
+Thanks for using this StackScript. Follow http://github.com/philipefarias/StackScripts for updates.
+
+--
+root
+Linode VPS "$vps_hostname"
+EOD
+fi
+
+reboot
---
+++ lib-system.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+#
+# System helper functions
+#
+# Author: Philipe Farias <philipefarias@gmail.com>
+
+source <ssinclude StackScriptID="1">
+
+function lower {
+    # helper function
+    echo $1 | tr '[:upper:]' '[:lower:]'
+}
+
+function setup_hostname {
+  HOSTNAME=$1
+  if [ -z "$HOSTNAME" ] ; then
+    export HOSTNAME=get_rdns_primary_ip
+  fi
+  HOST=`echo $HOSTNAME | sed 's/\(\[a-z0-9\]\)*\..*/\1/'`
+  echo "$HOST" >  /etc/hostname
+  echo "`system_primary_ip` $HOSTNAME $HOST" >> /etc/hosts
+  sed -i "/(SET_HOSTNAME='yes')/#\1/" /etc/default/dhcpcd
+  start hostname
+}
+
+function update_locale_en_US_UTF_8 {
+  # locale-gen en_US.UTF-8
+  dpkg-reconfigure locales
+  update-locale LANG=en_US.UTF-8
+}
+
+function install_postfix {
+  # $1 - root email
+  # $2 - username
+  postfix_install_loopback_only # SS1
+  #install mail sending utilities
+  apt-get -y install mailutils
+  #configure root alias
+  echo "root: $1" >> /etc/aliases
+  echo "$2: root" >> /etc/aliases
+  cat /etc/hostname > /etc/mailname
+  newaliases
+  sed -i "s/mydestination = localhost, localhost.localdomain, , localhost/mydestination = localhost, localhost.localdomain, $HOSTNAME/" /etc/postfix/main.cf
+  service postfix restart
+}
+
+# SSH functions
+function configure_sshd {
+  sshd_config_set_port "$SSHD_PORT"
+  sshd_config_permitrootlogin "$SSHD_PERMITROOTLOGIN"
+  sshd_config_passwordauthentication "$SSHD_PASSWORDAUTH"
+  sshd_config_pubkeyauthentication "$SSHD_PUBKEYAUTH"
+  service ssh restart
+}
+
+function add_user_ssh_key {
+    # $1 - username
+    # $2 - ssh key
+    USERNAME=`lower $1`
+    USER_HOME=`system_get_user_home "$USERNAME"`
+    sudo -u "$USERNAME" mkdir "$USER_HOME/.ssh"
+    sudo -u "$USERNAME" touch "$USER_HOME/.ssh/authorized_keys"
+    sudo -u "$USERNAME" echo "$2" >> "$USER_HOME/.ssh/authorized_keys"
+    chmod 0600 "$USER_HOME/.ssh/authorized_keys"
+}
+
+function sshd_config_set_port {
+  sed -i "s/Port 22/Port $1/" /etc/ssh/sshd_config
+}
+
+function sshd_config_edit_bool {
+    # $1 - param name
+    # $2 - Yes/No
+    VALUE=`lower $2`
+    if [ "$VALUE" == "yes" ] || [ "$VALUE" == "no" ]; then
+        sed -i "s/^#*\($1\).*/\1 $VALUE/" /etc/ssh/sshd_config
+    fi
+}
+
+function sshd_config_permitrootlogin {
+    sshd_config_edit_bool "PermitRootLogin" "$1"
+}
+
+function sshd_config_passwordauthentication {
+    sshd_config_edit_bool "PasswordAuthentication" "$1"
+}
+
+function sshd_config_pubkeyauthentication {
+    sshd_config_edit_bool "PubkeyAuthentication" "$1"
+}
+
+function sshd_config_passwordauthentication {
+    sshd_config_edit_bool "PasswordAuthentication" "$1"
+}
+
+# Monit and Munin
+function install_monit {
+  # $1 - root email
+  apt-get -y install monit
+  sed -i 's/startup=0/startup=1/' /etc/default/monit
+  mkdir -p /etc/monit/conf.d/
+  sed -i "s/# set daemon  120/set daemon 120/" /etc/monit/monitrc
+  sed -i "s/#     with start delay 240/with start delay 240/" /etc/monit/monitrc
+  sed -i "s/# set logfile syslog facility log_daemon/set logfile \/var\/log\/monit.log/" /etc/monit/monitrc
+  sed -i "s/# set mailserver mail.bar.baz,/set mailserver localhost/" /etc/monit/monitrc
+  sed -i "s/# set eventqueue/set eventqueue/" /etc/monit/monitrc
+  sed -i "s/#     basedir \/var\/monit/basedir \/var\/monit/" /etc/monit/monitrc
+  sed -i "s/#     slots 100 /slots 100/" /etc/monit/monitrc
+  sed -i "s/# set alert sysadm@foo.bar/set alert $1 reminder 180/" /etc/monit/monitrc
+  sed -i "s/# set httpd port 2812 and/ set httpd port 2812 and/" /etc/monit/monitrc
+  sed -i "s/#     use address localhost/use address localhost/" /etc/monit/monitrc
+  sed -i "s/#     allow localhost/allow localhost/" /etc/monit/monitrc
+  sed -i "s/# set mail-format { from: monit@foo.bar }/set mail-format { from: monit@`hostname -f` }/" /etc/monit/monitrc
+  cat << EOT > /etc/monit/conf.d/system
+  check system `hostname`
+    if loadavg (1min) > 4 then alert
+    if loadavg (5min) > 4 then alert
+    if memory usage > 90% then alert
+    if cpu usage (user) > 70% then alert
+    if cpu usage (system) > 30% then alert
+    if cpu usage (wait) > 20% then alert
+
+check filesystem rootfs with path /
+if space > 80% then alert
+EOT
+}
+
+function install_munin_node {
+  # $1 - node hostname
+  # $2 - munin server ip
+  apt-get -y install munin-node
+  sed -i "s/host_name .*/host_name $1/" /etc/munin/munin-node.conf
+  sed -i "s/allow .*/allow \^$2\$/" /etc/munin/munin-node.conf
+  service munin-node restart
+}
